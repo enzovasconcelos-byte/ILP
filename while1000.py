d4c6b30df438c56a6bca1ee01b2233450754bcf0
Vaconcelos14 <vasconcelos14ia@gmail.com>
2026-04-23 14:42:48 +0000

    Atividae while 1 a 8 e a 1000

---
+++ while1000.py
@@ -0,0 +1,7 @@
+x = 1
+
+
+
+while x < 100000000000000000000000000000000000:
+    print(f"Aura de João Gabriel depois de pegar a My Little Poney:{x}")
+    x = x-1
---
+++ while7.py
@@ -0,0 +1,11 @@
+x = 1
+while x <= 20:
+    # print(x)
+    # x = (x)* 3
+    # if x 
+    # problema
+    if x % 3 == 0:
+        print(x)
+
+    # incremento
+    x = x + 1
